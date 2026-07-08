5137127e7f68be37a7b55e1540c4f726058f983d
Kasper Landsvig <282058394+Landsvig1@users.noreply.github.com>
2026-07-08 23:15:46 +0200

    fix: redirect www to non-www as a fallback in next.config

--- next.config.ts
+++ next.config.ts
@@ -88,6 +88,21 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.vibetrends.dk',
+          },
+        ],
+        destination: 'https://vibetrends.dk/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
